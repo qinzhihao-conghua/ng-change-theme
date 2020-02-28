75038c874afe7e999508fe12c52f71fd311ece58
qinzhihao-conghua <1043419697@qq.com>
2020-02-28 15:57:25 +0800

    基本完成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MiddleComponent } from './middle/middle.component';
+import { BottomComponent } from './bottom/bottom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MiddleComponent,
+    BottomComponent
   ],
   imports: [
     BrowserModule
